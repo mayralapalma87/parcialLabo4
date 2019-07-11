51cfac92f9bffdc982b54c8307d88df8a1f179b9
MayraLapalma <mayralapalma87@hotmail.com>
2019-07-11 16:50:12 -0300

    se agrega modal

--- src/app/modalBusqueda/modalBusqueda.component.ts
+++ src/app/modalBusqueda/modalBusqueda.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserInterface } from '../models/user';
+import { DataApiService } from '../services/data-api.service';
 
 @Component({
   selector: 'app-modalBusqueda',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modalBusqueda.component.css']
 })
 export class ModalBusquedaComponent implements OnInit {
-
-  constructor() { }
+  public arrayDeUsuariosOriginalParaFiltrar: UserInterface[] = [];
+  public usuariosModal: UserInterface[] = [];
+  constructor(private dataApi: DataApiService) { }
 
   ngOnInit() {
   }
 
+  getUsuarios() {
+    this.dataApi.getUsers().subscribe( usuarios => {
+      this.arrayDeUsuariosOriginalParaFiltrar = usuarios;
+    });
+  }
+  //Metodos de busqueda:
+  mostrarDatos(Usuarios) {
+    console.log(Usuarios);
+    this.usuariosModal = Usuarios;
+  }
+  public reiniciar() {
+    this.usuariosModal = [];
+  }
+
+  seBorro(id: any) {
+    this.usuariosModal = this.arrayDeUsuariosOriginalParaFiltrar.filter( usuario => usuario.id !== id);
+  }
 }
